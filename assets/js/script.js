8ab86e799e57642b8e5439a9427d2d1f0ccb6cda
mrjannajafi <njfymrjan@gmail.com>
2025-03-09 04:41:40 -0700

    project is  completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,61 @@
-const textInp = document.querySelector("#textInp");
-const totalResult = document.querySelector("#totalResult");
-const wordResult = document.querySelector("#wordResult");
-const sentenceResult = document.querySelector("#sentenceResult");
-const timeResult = document.querySelector("#timeResult");
-const toggleThemeBtn = document.getElementById("toggleTheme");
-const bodyElement = document.body;
+window.addEventListener("DOMContentLoaded", function () {
+  const textInp = document.getElementById("textInp");
+  const totalResult = document.getElementById("totalResult");
+  const wordResult = document.getElementById("wordResult");
+  const sentenceResult = document.getElementById("sentenceResult");
+  const timeResult = document.getElementById("timeResult");
+  const toggleThemeBtn = document.getElementById("toggleTheme");
+  const btnIcon = this.document.getElementById("btnIcon");
 
-toggleThemeBtn.addEventListener("click", () => {
-  bodyElement.classList.toggle("dark");
-});
+  const countCharacter = function (text) {
+    const characters = text.split("");
+    totalResult.textContent = characters.length.toString().padStart(2, "0");
+  };
 
-const countCharacter = function (txt) {
-  const characters = txt.split("");
-  totalResult.textContent = characters.length.toString().padStart(2, "0");
-};
-const countWord = function (txt) {
-  if (!txt) return 0;
-  const words = txt.split(" ");
-  wordResult.textContent = words.length.toString().padStart(2, "0");
-};
-const countSentence = function (txt) {
-  const sentences = txt.split(/[.?!\n]+/);
-  sentenceResult.textContent = (sentences.length - 1)
-    .toString()
-    .padStart(2, "0");
-};
-const countTime = function (txt) {
-  const words = txt.split(" ");
-  const time = words.length / 200;
-  const seconds = Math.floor(time * 60);
-  const hour = Math.floor(time / 60);
+  const countWord = function (text) {
+    if (!text) return 0;
+    const words = text.split(" ");
+    wordResult.textContent = words.length.toString().padStart(2, "0");
+  };
 
-  if (time < 1) {
-    timeResult.textContent = `${seconds}sec`;
-  } else if (time < 60) {
-    const sec = Math.floor(seconds % 60);
-    const min = Math.floor(time);
-    timeResult.textContent = `${min}min ${sec}sec`;
-  } else {
-    const sec = Math.floor(seconds % 60);
-    const min = Math.floor(time % 60);
+  const countSentence = function (text) {
+    const sentences = text.split(/[.?!\n]+/);
+    sentenceResult.textContent = (sentences.length - 1)
+      .toString()
+      .padStart(2, "0");
+  };
 
-    timeResult.textContent = `${hour}hour ${min}min ${sec}sec`;
-  }
-};
+  const countTime = function (text) {
+    const words = text.split(" ");
+    const time = words.length / 200;
+    const seconds = Math.floor(time * 60);
+    const hour = Math.floor(time / 60);
 
-textInp.addEventListener("input", function () {
-  const text = textInp.value;
-  countCharacter(text);
-  countSentence(text);
-  countWord(text);
-  countTime(text);
+    if (time < 1) {
+      timeResult.textContent = `${seconds}sec`;
+    } else if (time < 60) {
+      const sec = Math.floor(seconds % 60);
+      const min = Math.floor(time);
+      timeResult.textContent = `${min}min ${sec}sec`;
+    } else {
+      const sec = Math.floor(seconds % 60);
+      const min = Math.floor(time % 60);
+
+      timeResult.textContent = `${hour}hour ${min}min ${sec}sec`;
+    }
+  };
+
+  textInp.addEventListener("input", function () {
+    const text = textInp.value;
+    countCharacter(text);
+    countSentence(text);
+    countWord(text);
+    countTime(text);
+  });
+
+  toggleThemeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnIcon.classList.toggle('fa-thin')
+    btnIcon.classList.toggle("fa-moon");
+  });
 });
---
+++ assets/js/script2.js
@@ -0,0 +1,29 @@
+let t = "";
+
+let n = 20;
+console.log("running");
+
+for (let i = 0; i < n; i++) {
+  for (let j = n; j > i; j--) {
+    t += " ";
+  }
+
+  for (let j = i; j > 0; j--) {
+    t += " *  ";
+  }
+  t += "\n";
+}
+
+for (let i = 0; i < n; i++) {
+  for (let j = i; j > 0; j--) {
+    t += " ";
+  }
+
+  for (let j = 0; j < n - i; j++) {
+    t += " *  ";
+  }
+  t += "\n";
+}
+
+console.log(t);
+
